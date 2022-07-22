9e329a1e84dbcd4be0405684341c8b908fdd068b
Vasily Mitronov <vasmitr95@gmail.com>
2022-07-22 14:03:50 +0000

    improve error messages

--- src/db/db.js
+++ src/db/db.js
@@ -4,26 +4,38 @@ class DB {
     this.transactions = transactions;
   }
 
+  getUser(userId) {
+    try {
+      return this.users.find((_user) => _user.userId === userId);
+    } catch (e) {
+      throw new Error(`User not found, ${e.text}`);
+    }
+  }
+
+  insertUser(user) {
+    try {
+      this.users = [...this.users.filter((_user) => _user.userId !== user.userId), { ...user }];
+    } catch (e) {
+      throw new Error(`Error while inserting user, ${e.text}`);
+    }
+  }
+
   setUserTransactionLastDate(userId, date) {
-    const user = this.users.find((_user) => _user.userId === userId);
+    const user = this.getUser(userId);
     user.lastTransactionDate = date;
-    this.users = [...this.users.filter((_user) => _user.userId !== userId), { ...user }];
+    this.insertUser(user);
   }
 
   addUserAmount(userId, amount) {
-    const user = this.users.find((_user) => _user.userId === userId);
+    const user = this.getUser(userId);
     user.totalAmount += amount;
-    this.users = [...this.users.filter((_user) => _user.userId !== userId), { ...user }];
+    this.insertUser(user);
   }
 
   dropUserAmount(userId) {
-    const user = this.users.find((_user) => _user.userId === userId);
+    const user = this.getUser(userId);
     user.totalAmount = 0;
-    this.users = [...this.users.filter((_user) => _user.userId !== userId), { ...user }];
-  }
-
-  getUser(userId) {
-    return this.users.find((_user) => _user.userId === userId);
+    this.insertUser(user);
   }
 
   getUserTransactionsPerDay(userId, date) {
